4e72aa9502004a5459450e08a63d5b2233a0eaea
İlker Yıldız <ilker_4@mail.com>
2021-09-04 16:19:28 +0300

    Redux files and folders added.

---
+++ src/store/rootReducer.js
@@ -0,0 +1,7 @@
+//Tüm state'leri topladığım yer.
+
+const rootReducer = combaneReducer({
+    // cart : cartReducer | yapabiliriz. Sebebi cartReducer diye çağırmak yerine direkt cart olarak kullanmak.
+})
+
+export default rootReducer;
